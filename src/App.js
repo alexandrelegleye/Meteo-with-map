23b4681997bfd59c7eb5159183370794cd9f5ef3
Alex <alexandre.legleye@gmail.com>
2022-09-22 18:59:32 +0400

    feat changestyle en cours/ state transmis mais pas de refresh

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import MapNavbar from './components/MapNavbar/MapNavbar'; 
 import Map from './components/Map/Map';
+import { useState } from 'react';
 
 import './App.css';
 
 function App() {
+
+  const [lng, setLng] = useState(55.471843);
+  const [lat, setLat] = useState(-21.332838);
+  const [zoom] = useState(14);
+  const [API_KEY] = useState('74e4eaf562dc4ca59197628b419c2e9c');
+  const [style, setStyle]= useState('toner-grey')
+
   return (
     <div className="App">
-    <MapNavbar />
-    <Map />
+    <MapNavbar 
+    onStyleChange={setStyle}
+    />
+    <Map
+    state={{lng, lat, zoom, API_KEY, style,}}
+    
+    />
     </div>
   );
 }
--- src/components/MapNavbar/MapNavbar.js
+++ src/components/MapNavbar/MapNavbar.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './MapNavbar.css';
 
-export default function MapNavbar(){
+export default function MapNavbar({
+  onStyleChange
+  }
+){
   return (
     <div className="heading">
     <h1>This is my map App</h1>
+    <button type="button" onClick={(e) => onStyleChange('osm-carto')}> 
+    click Me
+    </button>
     </div>
   );
 }
 
 
-/*  { <button onClick={handleMapStyle() => ''}> } */
-
-/*</button>  */
