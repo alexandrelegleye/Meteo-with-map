3db4b75e64d91fe1b84fb4fd46d387b6be3c7849
Alex <alexandre.legleye@gmail.com>
2022-10-14 12:40:10 +0400

    request weather in other file

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,28 +1,27 @@
 import React, { useEffect } from "react";
-import axios from "axios";
 import CurrentWeather from "../CurrentWeather/CurrentWeather";
 import HourlyWeather from "../HourlyWeather/HourlyWeather";
 import DailyWeatherCard from "../DailyWeather/DailyWeatherCard";
-import "./WeatherStyle.css";
 import { Segment } from "semantic-ui-react";
 import { useRecoilValue, useRecoilState } from "recoil";
 import { weatherDataNeeded } from "../../atomes/weatherSelector";
 import { weatherState } from "../../atomes/weatherAtoms";
-// import fetchWeatherData from "../../requests/weatherRequest";
+import FetchDataRequest from "../../requests/weatherRequest";
+
+// style
+import "./WeatherStyle.css";
 
 function Weather() {
 
   const {lng, lat, firstInput} = useRecoilValue(weatherDataNeeded)
-
-  //const [weatherData, setWeatherData] = useState("");
   const [weatherDataRecoil, setWeatherDataRecoil] = useRecoilState(weatherState);
 
-  const FetchData = async(lat, lng) => {
+  const FetchData = async (lat, lng) => {
     try{
+      /*       console.log("Fetch", await FetchDataRequest(lat,lng));
       const response = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&hourly=temperature_2m,precipitation,rain,cloudcover,windspeed_10m,winddirection_10m,direct_radiation_instant&daily=precipitation_sum,weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&timezone=auto`)
-      console.log(response.data)
-      // setWeatherData(response.data)
-      setWeatherDataRecoil(response.data)
+      console.log(response.data) */
+      setWeatherDataRecoil(await FetchDataRequest(lat,lng))
     } 
     catch (error) {
       console.log(error);
@@ -31,8 +30,6 @@ function Weather() {
 
   useEffect(() => {
     FetchData(lat, lng)
-    // fetchWeatherData(lat,lng)
-    console.log("In Use effect: new weather Fetch");
   },[lat, lng, firstInput])
 
   return (
--- src/requests/weatherRequest.js
+++ src/requests/weatherRequest.js
@@ -1,22 +1,18 @@
-import { useSetRecoilState } from "recoil";
-import { weatherState } from "../atomes/weatherAtoms";
 import axios from "axios";
 
-const setWeatherDataRecoil = useSetRecoilState(weatherState);
-
-
 // todo PB de hook
-const FetchData = async(lat, lng) => {
+const FetchDataRequest = async (lat, lng) => {
+  console.log(lat, lng);
 
   const response = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&hourly=temperature_2m,precipitation,rain,cloudcover,windspeed_10m,winddirection_10m,direct_radiation_instant&daily=precipitation_sum,weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&timezone=auto`)
   try{
     console.log(response.data)
     // setWeatherData(response.data)
-    setWeatherDataRecoil(response.data)
+    return response.data
   } 
   catch (error) {
     console.log(error);
   }
 } 
 
-export default FetchData; 
+export default FetchDataRequest; 
