23b09252eec9bba1a01e1a54fe5162d7eb13bfab
alexandrelegleye <91343906+alexandrelegleye@users.noreply.github.com>
2022-10-18 17:49:48 +0400

    log to check rain value

--- src/requests/weatherRequest.js
+++ src/requests/weatherRequest.js
@@ -12,7 +12,7 @@ const FetchDataRequest = async (lat, lng) => {
         lang: "fr",
       },
     })  
-    // console.log(response.data)
+    console.log(response.data)
     return response.data
   } 
   catch (error) {
